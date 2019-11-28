fba67c16f6bff29f83bf525838794b87ef6808bd
elbowjonze <elbowjonze@gmail.com>
2019-11-27 20:12:09 -0500

    Added movement mapping function

--- server.R
+++ server.R
@@ -1,5 +1,79 @@
 shinyServer(function(input, output, session) {
     
+    ## initial conditions
+    init_loc <- data.frame("char" = c('Alex', 'Ivan'),
+                           "xloc" = c(1, 5),
+                           "yloc" = c(1, 5)
+    )
+    
+    alex_pos <<- subset(init_loc, char=='Alex')
+    print('alex_pos')
+    print(alex_pos)
+    
+    ivan_pos <<- subset(init_loc, char=='Ivan')
+
+    
+    ## calculate valid movement spaces
+    move_map <- function(steps, x, y)
+    {
+        valid_moves <- NULL
+        valid_moves <- data.frame('x'=integer(), 'y'=integer(), 'cell'=character())
+        for(i in 1:steps)
+        {
+            ## move orthoginally
+            valid_moves <- rbind(valid_moves,
+                                 data.frame('x' = c(x+i, x-i, x, x),
+                                            'y' = c(y, y, y+i, y-i),
+                                            'cell' = c(paste0(x+i, '.', y),
+                                                       paste0(x-i, '.', y),
+                                                       paste0(x,   '.', y+i),
+                                                       paste0(x,   '.', y-i)
+                                            )
+                                 )
+            )
+            
+            ## one step diagonally
+            if(i == 2)
+            {
+                valid_moves <- rbind(valid_moves,
+                                     data.frame('x' = c(x+1, x+1, x-1, x-1),
+                                                'y' = c(y+1, y-1, y-1, y+1),
+                                                'cell' = c(paste0(x+1, '.', y+1),
+                                                           paste0(x+1, '.', y-1),
+                                                           paste0(x-1, '.', y-1),
+                                                           paste0(x-1, '.', y+1)
+                                                )
+                                     )
+                )
+            }
+            
+            ## move like a knight
+            if(i == 3)
+            {
+                valid_moves <- rbind(valid_moves,
+                                     data.frame('x' = c(x+2, x+2, x-2, x-2, x+1, x-1, x+1, x-1),
+                                                'y' = c(y+1, y-1, y-1, y+1, y+2, y+2, y-2, y-2),
+                                                'cell' = c(paste0(x+2, '.', y+1), 
+                                                           paste0(x+2, '.', y-1),
+                                                           paste0(x-2, '.', y-1),
+                                                           paste0(x-2, '.', y+1),                                 
+                                                           paste0(x+1, '.', y+2),
+                                                           paste0(x-1, '.', y+2),
+                                                           paste0(x+1, '.', y-2),
+                                                           paste0(x-1, '.', y-2))
+                                     )
+                )
+            }
+        }
+        
+        valid_moves$x <- as.integer(valid_moves$x)
+        valid_moves$y <- as.integer(valid_moves$y)
+        valid_moves <- subset(valid_moves, x >=1 & x<= 8 & y >=1 & y<=8)
+        
+        return(valid_moves)
+    }
+        
+        
     ## polygons
     gpoly <- NULL
     for(x in 0:7)
@@ -17,13 +91,71 @@ shinyServer(function(input, output, session) {
     gpoly <- as.data.frame(gpoly)
     names(gpoly) <- c('x', 'y', 'cell')
     
-    
+    ## initial grid
     output$playgrid <- renderPlot({
         ggplot() +
             geom_polygon(data=gpoly, mapping=aes(x=x, y=y, group=cell), color='black', fill=NA) +
             annotation_raster(alex, xmin=1, xmax=2, ymin=1, ymax=2) +
             annotation_raster(ivan, xmin=5, xmax=6, ymin=5, ymax=6)
     })
+    
+    
+    ## -------------------------------
+    ## REACTIVE STUFF
+    ## -------------------------------
+    char_selected <- reactiveValues()
+    
+    
+    
+    ## user char selection
+    observeEvent(input$grid_click$x,{
+        ## select Alex, update plot
+        if(floor(input$grid_click$x)==alex_pos$xloc & floor(input$grid_click$y)==alex_pos$xloc)
+        {
+            ## calculate valid moves
+            #curr_loc <- paste0(alex_pos$xloc, '.', alex_pos$yloc)
+            moves <- move_map(3, alex_pos$xloc, alex_pos$yloc)
+            print(moves)
+            
+            
+            output$playgrid <- renderPlot({
+                ggplot() +
+                    geom_polygon(data=gpoly, mapping=aes(x=x, y=y, group=cell), color='black', fill=NA) +
+                    geom_polygon(data=subset(gpoly, cell %in% moves$cell), 
+                                 mapping=aes(x=x, y=y, group=cell), 
+                                 color='black', 
+                                 fill='#ccff99') +
+                    annotation_raster(alex, xmin=alex_pos$xloc, xmax=alex_pos$xloc+1, ymin=alex_pos$yloc, ymax=alex_pos$yloc+1) +
+                    annotation_raster(ivan, xmin=ivan_pos$xloc, xmax=ivan_pos$xloc+1, ymin=ivan_pos$yloc, ymax=ivan_pos$yloc+1)
+            })            
+            
+        } 
+
+        ## select Ivan, update plot
+        if(floor(input$grid_click$x)==ivan_pos$xloc & floor(input$grid_click$y)==ivan_pos$xloc)
+        {
+            ## calculate valid moves
+            #curr_loc <- paste0(ivan_pos$xloc, '.', ivan_pos$yloc)
+            moves <- move_map(2, ivan_pos$xloc, ivan_pos$yloc)
+            print(moves)
+            
+            
+            output$playgrid <- renderPlot({
+                ggplot() +
+                    geom_polygon(data=gpoly, mapping=aes(x=x, y=y, group=cell), color='black', fill=NA) +
+                    geom_polygon(data=subset(gpoly, cell %in% moves$cell), 
+                                 mapping=aes(x=x, y=y, group=cell), 
+                                 color='black', 
+                                 fill='#ccff99') +
+                    annotation_raster(alex, xmin=alex_pos$xloc, xmax=alex_pos$xloc+1, ymin=alex_pos$yloc, ymax=alex_pos$yloc+1) +
+                    annotation_raster(ivan, xmin=ivan_pos$xloc, xmax=ivan_pos$xloc+1, ymin=ivan_pos$yloc, ymax=ivan_pos$yloc+1)
+            })            
+            
+        }                
+    })
+            
+    
+    
     
     output$cell_click <- renderPrint({
         xval <- floor(input$grid_click$x)
--- ui.R
+++ ui.R
@@ -1,4 +1,5 @@
 options(stringsAsFactors = FALSE)
+setwd('D:/R_files/shiny/shinyforce/')
 
 library(shiny)
 library(shinydashboard)
@@ -9,19 +10,48 @@ library(png)
 alex <- readPNG('D:/R_files/shiny/shinyforce/sprites/alex1.png')
 ivan <- readPNG('D:/R_files/shiny/shinyforce/sprites/ivan1.png')
 
-fluidPage(
-    fluidRow(
-        column(width = 4,
-               plotOutput("playgrid", height=600, width=600,
-                          click = "grid_click"
-               )
-        )
-    ),
-    fluidRow(
-        column(width = 6,
-               h4("Return cell"),
-               verbatimTextOutput("cell_click")
-        )
+
+header <- dashboardHeader(title='SHINY FORCE')
+
+
+sidebar <- dashboardSidebar(
+    sidebarMenu(
+        menuItem("", tabName="game", icon = icon("dashboard")),
+        menuItem("I Dunno", icon=icon("th"), tabName = "dunno")
     )
 )
 
+
+body <- dashboardBody(
+    
+    ## suppress all error messages
+    tags$style(type="text/css",
+               ".shiny-output-error { visibility: hidden; }",
+               ".shiny-output-error:before { visibility: hidden; }"
+    ),
+    
+    tabItems(
+        tabItem(tabName='game',
+            h2('GAME'),
+            fluidRow(
+                column(width = 4,
+                    plotOutput("playgrid", height=600, width=600,
+                                click = "grid_click"
+                    )
+                )
+            ),
+            fluidRow(
+                column(width = 6,
+                    h4("Return cell"),
+                    verbatimTextOutput("cell_click")
+                )
+            )
+        ),
+        tabItem(tabName='dunno',
+                h2('meh')
+        )        
+    )
+)
+    
+suppressWarnings(dashboardPage(header, sidebar, body))                            
+    
